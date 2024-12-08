86f38c4c7328f03a38c64fdd1c65f30db22d4711
Gustavo Silva <prazeresdasilvag@gmail.com>
2024-12-08 16:11:11 -0300

    [200~

--- src/css/js/index.js
+++ src/css/js/index.js
@@ -1,3 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+        offset: 200,
+        duration: 1200,
+    });
+});
+
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
 const botaoEsconderProjetos = document.querySelector('.btn-esconder-projetos')
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
@@ -29,6 +36,13 @@ function esconderBotaoMenosProjetos() {
 function mostrarMaisProjetos() {
     projetosInativos.forEach(projetoInativo => {
         projetoInativo.classList.add('ativo');
+
+        var elementosAOS = projetos.querySelectorAll('[data-aos]');
+        elementosAOS.forEach(function (el) {
+            el.classList.remove('aos-animate');
+        });
+
+        AOS.refresh();
     });
 }
 
@@ -36,4 +50,22 @@ function mostrarMenosProjetos() {
     projetosInativos.forEach(projetoInativo => {
         projetoInativo.classList.remove('ativo');
     });
+    document.getElementById('projetos').scrollIntoView()
 }
+
+function verificarSecaoVisivel() {
+    const secaoHome = document.getElementById('home');
+    const secaoSobre = document.getElementById('sobre');
+    const botaoHome = document.querySelector('.botao-home');
+
+    const posicaoSecaoHome = secaoHome.getBoundingClientRect();
+    const posicaoSecaoSobre = secaoSobre.getBoundingClientRect();
+
+    if (posicaoSecaoHome.top < window.innerHeight && posicaoSecaoHome.bottom > 0) {
+        botaoHome.style.display = 'none';
+    } else if (posicaoSecaoSobre.top < window.innerHeight && posicaoSecaoSobre.bottom > 0) {
+        botaoHome.style.display = 'block';
+    }
+};
+
+window.addEventListener('scroll', verificarSecaoVisivel);
